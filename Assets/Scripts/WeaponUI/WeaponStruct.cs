796118ddff97194024658fd6d2ca0e3a89770908
jinhachun <jinhachun@gmail.com>
2024-09-07 10:11:06 +0900

    [feat] 짭랑이 수치조정 / 큐브시스템 틀만 만들어놓음
    weaponstruct에 weaponCubeStruct를 List로 만들어 놓고, CubePanel에서 큐브 옵션 정할 때 랜덤하게 waeponCubeStruct를 생성해서 해당 weapon에 넣어ㅏ줌

--- Assets/Scripts/WeaponUI/WeaponStruct.cs
+++ Assets/Scripts/WeaponUI/WeaponStruct.cs
@@ -9,5 +9,21 @@ public struct WeaponStruct
     public string _name;
     public string _skillName;
     public string _skillInfo;
+    public List<WeaponCubeStruct> _cubeOption;
 
+}
+[System.Serializable]
+public struct WeaponCubeStruct
+{
+    public WeaponCubeStat _stat;
+    public WeaponCubeTier _tier;
+    public int _option;
+}
+public enum WeaponCubeStat
+{
+    Hp,Att,Def,Acc,Eva,Sla,Stk,Pen,Ran
+}
+public enum WeaponCubeTier
+{
+    Legendary,Epic,Common
 }
